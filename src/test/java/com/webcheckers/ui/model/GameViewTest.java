2066e32c76e080892bdcc5c113feef82a9e50f3b
Tony <tj2561@rit.edu>
2020-07-02 08:34:53 -0700

    added @Test

--- src/test/java/com/webcheckers/ui/model/GameViewTest.java
+++ src/test/java/com/webcheckers/ui/model/GameViewTest.java
@@ -10,15 +10,26 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+/**
+ * Tests the GameView class
+ */
 public class GameViewTest {
     final Player p1 = new Player("p1");
     final Player p2 = new Player("p2");
     final GameView CuT = new GameView(p1, p2, Piece.Color.RED);
 
+    /**
+     * Checks if the board is null
+     */
+    @Test
     public void testBoard() {
         assertNotNull(CuT);
     }
 
+    /**
+     * Check if the board is properly set up.
+     */
+    @Test
     public void testSetUp() {
         CuT.setupBoard();
         final List board = CuT.getBoard();
@@ -26,6 +37,10 @@ public class GameViewTest {
 
     }
 
+    /**
+     * Test if the baord recognizes player1's color.
+     */
+    @Test
     public void testColor() {
         assertTrue(p1.getColor() == CuT.getPlayerColor());
     }
