3faa823cc38a7b12e14603e9f8ae96dc5de30018
Tony <tj2561@rit.edu>
2020-07-09 08:24:00 -0700

    Created unit test for validator

---
+++ src/test/java/com/webcheckers/model/ValidatorTest.java
@@ -0,0 +1,32 @@
+package com.webcheckers.model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ValidatorTest {
+    GameView view = new GameView(Piece.Color.RED);
+    final Validator validator = new Validator();
+    String data1 ="{\"start\":{\"row\":0,\"cell\":0},\"end\":{\"row\":1,\"cell\":1}}";
+    String data2 ="{\"start\":{\"row\":5,\"cell\":6},\"end\":{\"row\":1,\"cell\":7}}";
+    Move goodMove = new Move(data1);
+    Move badMove = new Move(data2);
+
+
+    @Test
+    public void nullTest() {
+        assertNotNull(validator, "validator is not null");
+    }
+
+    @Test
+    public void moveTestGood() {
+        assertEquals(validator.validateMove(view.getBoard(), goodMove), "True");
+    }
+
+    @Test
+    public void moveTestBad() {
+        assertEquals(validator.validateMove(view.getBoard(), badMove), "False");
+    }
+
+}
